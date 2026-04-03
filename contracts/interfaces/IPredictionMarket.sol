28ec6be745d5021baa0cf962687c572ac2c31645
0xfps <anthony.nnaemeka.umeh@gmail.com>
2026-04-03 17:43:31 +0100

    feat: add interfaces.

---
+++ contracts/interfaces/IPredictionMarket.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IPredictionMarket {
+    event FeesCollected(address indexed recipient, uint256 amount);
+    event MarketInvalidated(uint256 timestamp);
+    event MarketResolved(bool indexed outcome, uint256 timestamp);
+    event Trade(
+        address indexed trader,
+        bool indexed isYes,
+        bool indexed isBuy,
+        uint256 shares,
+        uint256 cost,
+        Prices newPrices
+    );
+    event WinningsClaimed(address indexed user, uint256 amount);
+
+    error Nimbus_AlreadyClaimed();
+    error Nimbus_InsufficientLiquidity();
+    error Nimbus_InvalidAmount();
+    error Nimbus_MarketAlreadyResolved();
+    error Nimbus_MarketClosed();
+    error Nimbus_MarketNotClosed();
+    error Nimbus_MarketNotResolved();
+    error Nimbus_NoWinnings();
+    error Nimbus_SlippageExceeded();
+    error Nimbus_TooEarly();
+    error Nimbus_TransferFailed();
+    error Nimbus_Unauthorized();
+
+    struct UserPosition {
+        uint256 yesBalance;
+        uint256 noBalance;
+    }
+
+    struct Prices {
+        uint16 yesPrice;
+        uint16 noPrice;
+    }
+
+    //  Slippage protection built in,
+    //  users should buy by spending `amount` or lesser.
+    function buy(
+        bool isYes,
+        uint256 amount,
+        uint256 shares
+    ) external;
+
+    function sell(
+        bool isYes,
+        uint256 shares,
+        uint256 minReturn, 
+        address receiver
+    ) external;
+    
+    function resolve(bool outcome) external;
+    function invalidate() external;
+    function claim() external returns (uint256 payout);
+    function claimRefund() external returns (uint256 refund);
+    
+    function getCurrentPrices() external view returns (Prices memory prices);
+
+    function getBuyQuote(bool isYes, uint256 shares) external view returns (uint256 amount, uint256 newPrice);
+    function getSellQuote(bool isYes, uint256 shares) external view returns (uint256 amount, uint256 newPrice);
+    
+    function getUserPosition(address user) external view returns (UserPosition memory);
+}
---
+++ contracts/interfaces/IPredictionMarketFactory.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { MarketCreationData, MarketType } from "../utils/Market.sol";
+
+interface IPredictionMarketFactory {
+    event MarketCreated(
+        address indexed marketAddress,
+        address indexed creator,
+        address indexed resolver,
+        string question,
+        uint16 category,
+        MarketType marketType,
+        uint256 endTime
+    );
+    event ResolverApproved(address indexed resolver);
+    event ResolverRevoked(address indexed resolver);
+
+    error BackdatedMarket();
+    error InvalidEndTime();
+    error InvalidDuration();
+    error ResolverNotApproved();
+    error Unauthorized();
+
+    function createBinaryMarket(MarketCreationData calldata marketCreationData) external returns (address market);
+    function getTotalMarketCount() external view returns (uint256);
+
+    function approveResolver(address resolver) external;
+    function revokeResolver(address resolver) external;
+    function setMinMarketDuration(uint24 duration) external;
+    function setMaxMarketDuration(uint24 duration) external;
+    function transferOwnership(address newOwner) external;
+}
